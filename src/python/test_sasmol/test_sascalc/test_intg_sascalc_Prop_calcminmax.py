387c1428782bd5c58e92ddfdd10f087783eb7e61
Joseph E. Curtis <madscatt@gmail.com>
2015-12-14 15:16:25 -0500

    fixed common paths for test_sascalc

--- src/python/test_sasmol/test_sascalc/test_intg_sascalc_Prop_calcminmax.py
+++ src/python/test_sasmol/test_sascalc/test_intg_sascalc_Prop_calcminmax.py
@@ -1,6 +1,5 @@
 '''
-    SASSIE: Copyright (C) 2011 Joseph E. Curtis, Ph.D. 
-	 Core-Testing: Copyright (C) 2011 Hailiang Zhang, Ph.D.
+    SASMOL: Copyright (C) 2011 Joseph E. Curtis, Ph.D. 
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,18 +15,17 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from sassie.core_testing.util import env, util
+from sasmol.test_sasmol.util import env, util
 
 from unittest import main, skipIf
 from mocker import Mocker, MockerTestCase
-from sassie.sasmol import sasmol
+import sasmol.sasmol as sasmol
 
 import numpy
 
 import os
 
-
-PdbPath = os.path.dirname(os.path.realpath(__file__))+'/../../data/pdb_common/'
+PdbPath = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','data','pdb_common')+os.path.sep
 
 class Test_sascalc_Prop_calcminmax(MockerTestCase): 
 
