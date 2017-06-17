7258d6870cc08df829ee072ddde0da07c0f60f69
Joseph E. Curtis <madscatt@gmail.com>
2017-06-17 09:03:09 -0400

    added HPC sasconfig

---
+++ src/python/sasconfig.py
@@ -0,0 +1,23 @@
+import os
+
+###     BEGIN SYSADMIN EDIT ###
+###     BEGIN SYSADMIN EDIT ###
+###     BEGIN SYSADMIN EDIT ###
+
+__level__ = 'WARNING'
+__level__ = 'DEBUG'
+
+## INSTALLATION PATH
+
+__installation_path__ = '/share/apps/local'
+__installation_bin_path__ = '/share/apps/local/bin'
+
+
+###     END SYSADMIN EDIT ###
+###     END SYSADMIN EDIT ###
+###     END SYSADMIN EDIT ###
+
+__bin_path__ = __installation_bin_path__
+
+
+
